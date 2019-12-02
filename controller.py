ecc2522ba6301dd3cbbc120603476beee8c3a896
Jacob G <jvg11@case.edu>
2019-12-02 03:12:36 +0000

    modified outgoing encrypted flow to prevent accidentally duplicating packets

--- controller.py
+++ controller.py
@@ -180,7 +180,7 @@ class SecureSwitchController(app_manager.RyuApp):
 						encryptor_mac = self.device_macs[encryptor_ip]
 						encryptor_port = self.device_ports[encryptor_mac]
 						
-						self.send_with_flow(dp, msg, in_port, mac_src, mac_dst, pkt_ip.dst, encryptor_port, self.switch_encrypted_mac, encryptor_mac, True)
+						self.send_with_flow(dp, msg, in_port, mac_src, mac_dst, pkt_ip.dst, encryptor_port, self.switch_encrypted_mac, encryptor_mac, True, 1)
 						
 						return
 						
@@ -189,7 +189,7 @@ class SecureSwitchController(app_manager.RyuApp):
 							
 						final_mac = self.device_macs[ip_dst]
 						
-						self.send_with_flow(dp, msg, in_port, mac_src, mac_dst, pkt_ip.dst, self.device_ports[final_mac], self.switch_local_mac, final_mac, True)
+						self.send_with_flow(dp, msg, in_port, mac_src, mac_dst, pkt_ip.dst, self.device_ports[final_mac], self.switch_local_mac, final_mac, True, 2)
 						
 						return
 						
@@ -201,7 +201,7 @@ class SecureSwitchController(app_manager.RyuApp):
 						
 						print "Outbound unencrypted on endnet", switch_endnet, " from:", pkt_ip.src, "to", pkt_ip.dst, "encrypting with", encryptor_ip, "on port", encryptor_port
 						
-						self.send_with_flow(dp, msg, in_port, mac_src, mac_dst, pkt_ip.dst, encryptor_port, self.switch_unencrypted_mac, encryptor_mac, True)
+						self.send_with_flow(dp, msg, in_port, mac_src, mac_dst, pkt_ip.dst, encryptor_port, self.switch_unencrypted_mac, encryptor_mac, True, 4)
 												
 						return
 						
@@ -210,14 +210,14 @@ class SecureSwitchController(app_manager.RyuApp):
 						print " -> src:", mac_src, "dst:", mac_dst
 						
 						#FIXME: this is causing duplicated packets!!!
-						self.send_with_flow(dp, msg, in_port, mac_src, mac_dst, pkt_ip.dst, 1, self.switch_local_mac, self.switch_interchange_mac)
+						self.send_with_flow(dp, msg, in_port, mac_src, mac_dst, pkt_ip.dst, 1, mac_src, self.switch_interchange_mac, True, 3)
 						
 						return
 						
 					else:
 						print "Dropped IP packet on endnet", switch_endnet, ":", pkt_ip, eth
 						
-						self.add_flow_entry(dp, 1, parser.OFPMatch(eth_type=0x800, in_port=in_port, ipv4_src=ip_src, eth_src=mac_src, eth_dst=mac_dst), [])
+						self.add_flow_entry(dp, 5, parser.OFPMatch(eth_type=0x800, in_port=in_port, ipv4_src=ip_src, eth_src=mac_src, eth_dst=mac_dst), [])
 						
 						return
 				else:
@@ -226,7 +226,7 @@ class SecureSwitchController(app_manager.RyuApp):
 		#if we received a packet of unknown protocol, defensively drop it
 		return
 		
-	def send_with_flow(self, dp, msg, in_port, orig_eth_src, orig_eth_dst, orig_ip_dst, out_port, new_eth_src, new_eth_dst, add_flow = False):
+	def send_with_flow(self, dp, msg, in_port, orig_eth_src, orig_eth_dst, orig_ip_dst, out_port, new_eth_src, new_eth_dst, add_flow = False, priority = 2):
 		ofproto = dp.ofproto
 		parser = dp.ofproto_parser
 		
@@ -243,7 +243,7 @@ class SecureSwitchController(app_manager.RyuApp):
 		dp.send_msg(parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data))
 		
 		if add_flow:
-			self.add_flow_entry(dp, 1, parser.OFPMatch(eth_type=0x800, in_port=in_port, ipv4_dst=orig_ip_dst, eth_src=orig_eth_src, eth_dst=orig_eth_dst), actions, 300)
+			self.add_flow_entry(dp, priority, parser.OFPMatch(eth_type=0x800, in_port=in_port, ipv4_dst=orig_ip_dst, eth_src=orig_eth_src, eth_dst=orig_eth_dst), actions, 300)
 		
 		return
 				
